ad221fc4f6fde42992e8d7522930684bd7fd9fbd
towtigers0608 <250029633@qq.com>
2019-10-20 23:26:17 +0800

    mysql 配置存在问题

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #使用mysql数据库
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myblog',
-        'USER': 'root',
-        'PASSWORD': 'intel.123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'myblog',
+#         'USER': 'root',
+#         'PASSWORD': 'intel.123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
